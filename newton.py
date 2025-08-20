d8551bb4ab91a19d3858ef351d239c53c2c532c5
sharyali05 <sharyali16@gmail.com>
2025-08-20 11:59:44 -0700

    Update newton.py

--- newton.py
+++ newton.py
@@ -4,13 +4,13 @@ def gradient(func, x, h=1e-5):
 
 
 #newton method function
-def newton_method(start, func, tol=1e-7, max_iter=1000, h=1e-5):
+def newton_method(start, func, max_iter=1000, h=1e-5):
     x = start
     for i in range(max_iter):
         grad = gradient(func, x, h)
         
         x_new = x - func(x) / grad
-        if abs(x_new - x) < tol:  #convergence check
+        if abs(x_new - x) < h:  #convergence check
             return x_new
         
         x = x_new
