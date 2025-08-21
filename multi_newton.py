9dd8228c340d40f6edf539cb6967c5ee61d10060
sharyali05 <sharyali16@gmail.com>
2025-08-21 10:49:18 -0700

    Fix multivariate newton code

--- multi_newton.py
+++ multi_newton.py
@@ -1,13 +1,13 @@
 def derivative(func, x, h=1e-5):
-    """Numerical first derivative."""
+    #First derivative
     return (func(x + h) - func(x - h)) / (2 * h)
 
 def second_derivative(func, x, h=1e-5):
-    """Numerical second derivative."""
+    #second derivative
     return (func(x + h) - 2*func(x) + func(x - h)) / (h**2)
 
 def newton_method(func, x0, tol=1e-6, max_iter=100):
-    """Newton's method for univariate optimization."""
+    #Newton's method for univariate optimization.
     x = x0
     iteration = 0
     
@@ -15,9 +15,24 @@ def newton_method(func, x0, tol=1e-6, max_iter=100):
         g = derivative(func, x)
         H = second_derivative(func, x)
         if H == 0:   #no division by zero
-            print("Hessian is zero, stopping.")
             break
         x = x - g / H
         iteration += 1
     
     return x
+
+import numpy as np
+def newton_multi_var(grad_F, H, x0, tol=1e-8, max_iter=100):
+    x = np.array(x0, dtype=float)
+    for i in range(max_iter):
+        g = grad_F(x)
+        Hx = H(x)
+        
+        delta = np.linalg.solve(Hx, g)
+
+        x_new = x - delta
+        if np.linalg.norm(delta) < tol:
+            return x_new
+        x = x_new
+    return x
+ 
